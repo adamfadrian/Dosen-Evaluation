8767bb8a5a896e02519db57678b6052d8ed2740a
adamfadrian <adamfadrian12@gmail.com>
2023-05-26 08:01:14 +0700

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+
+module.exports =  {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -7,6 +8,10 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        "dark-alta": "#19345E",
+        "orange-alta": "#F47522",
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
@@ -14,5 +19,33 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [require('daisyui')],
+  daisyui: {
+    base: false,
+    darkTheme: "light",
+    themes: [
+      {
+        mytheme: {
+
+          "primary": "#19345E",
+
+          "secondary": "#0891b2",
+
+          "accent": "#F47522",
+
+          "neutral": "#CCCCCC",
+
+          "base-100": "#FFFFFF",
+
+          "info": "#8DCAC1",
+
+          "success": "#9DB787",
+
+          "warning": "#FFD25F",
+
+          "error": "#FC9581",
+        },
+      },
+    ]
+  },
 }
